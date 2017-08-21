db622c3ac6357dcc8b97eff9cbc7061a301d0a6d
chinmaydalvi <chinmaydalvi94@gmail.com>
2017-08-22 01:15:12 +0530

    passes the data between parent component App to child component video_list

--- src/index.js
+++ src/index.js
@@ -5,28 +5,34 @@ import YTSearch from 'youtube-api-search';
 
 // Importing Created Modules
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAPuPEJqh_gqHnA8KBTDZk0WSSrHxiDxOI';
 
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-});
-
 // Create New component this component can produce HTML
-
 class App extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=> {
+			 this.setState({ videos });  //SIMILAR to this.setState({ videos: videos });
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<SearchBar/>
+				<VideoList videos={this.state.videos}/>
 			</div>
 		);
 	}
 }
 
 // Take th component generated HTML and put it on the Page means in the DOM
-
 ReactDOM.render(<App />, document.querySelector('.container'));
 
 
