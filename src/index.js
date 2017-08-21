140d429bd97bf8f7bf698df7dc9994c636923096
chinmaydalvi <chinmaydalvi94@gmail.com>
2017-08-21 23:46:32 +0530

    convert functionbase compoment to class base component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 // NPM Modules
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // Importing Created Modules
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAPuPEJqh_gqHnA8KBTDZk0WSSrHxiDxOI';
 
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
+
 // Create New component this component can produce HTML
 
-const App = () => {
-	return (
-						<div>
-						<SearchBar/>
-						</div>
-				 )
+class App extends Component{
+	render() {
+		return (
+			<div>
+				<SearchBar/>
+			</div>
+		);
+	}
 }
 
 // Take th component generated HTML and put it on the Page means in the DOM
